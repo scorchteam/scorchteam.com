77452d08789088339e03a3af4e64206a04e99975
nicholasprussen <nicholasprussen@u.boisestate.edu>
2022-01-04 19:02:51 -0800

    Homepage implemented

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
 import "./Header.scss";
 import logo from "../../images/logo/scorchteam_logo.svg";
 import { useLocation } from "react-router-dom";
+import { animated, useSpring } from '@react-spring/web';
 
 const Header = (props) => {
     const tabs = ["home", "about", "projects", "contact"];
     const currentTab = useLocation().pathname;
+    const [collapsed, setCollapsed] = useState(true);
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', updateWindowDimensions);
+        updateWindowDimensions();
+
+        return () => {
+            window.removeEventListener('resize', updateWindowDimensions);
+        }
+    }, [])
+
+    const updateWindowDimensions = () => {
+        setWidth(window.innerWidth);
+    }
 
     const renderNavLinks = () => {
         const navLinks = tabs.map((tab) => {
@@ -13,7 +30,7 @@ const Header = (props) => {
                 hrefText = "/";
             }
             return (
-                <a  key={tab}
+                <a key={tab}
                     href={hrefText}
                     className={`navigation-link ${currentTab === hrefText ? 'active-nav' : ''}`}>
                     {tab}
@@ -24,20 +41,57 @@ const Header = (props) => {
     }
 
     return (
-        <header>
-            <div className="container-lg header">
-                <div className="header-left-section">
-                    <a href="/"><img className="header-logo" src={logo} alt="Scorch Team Logo"></img></a>
+        <div className="header-container">
+            <header>
+                <div className="container-lg header">
+                    <div className="header-left-section">
+                        <a href="/"><img className="header-logo" src={logo} alt="Scorch Team Logo"></img></a>
+                    </div>
+                    {
+                        width >= 768 &&
+                        <div className="header-middle-section">
+                            {renderNavLinks()}
+                            <a href="https://www.github.com/scorchteam" className="github-link">
+                                <i className="fab fa-github"></i>
+                                GitHub
+                            </a>
+                        </div>
+                    }
+                    {
+                        width < 768 &&
+                        <div className="header-middle-section">
+                            <div className="hamburger-icon">
+                                <i className="fas fa-hamburger hamburger-icon" onClick={() => { setCollapsed(!collapsed) }}></i>
+                            </div>
+                        </div>
+                    }
                 </div>
-                <div className="header-middle-section">
-                    {renderNavLinks()}
-                    <a href="https://www.github.com/scorchteam" className="github-link">
-                        <i class="fab fa-github"></i>
-                        GitHub
-                    </a>
-                </div>
+            </header>
+            <div className="header-bottom-section-container">
+                <animated.div
+                    style={useSpring({
+                        opacity: collapsed ? 1 : 0,
+                        y: collapsed ? 0 : -24,
+                        height: collapsed ? 184 : 0,
+                        config: {
+                            duration: 300
+                        }
+                    })}>
+                    <div className={`header-bottom-section`}>
+
+                        {renderNavLinks()}
+
+                        {/* {
+                    !collapsed &&
+                    width < 768 &&
+                    <div>fucky</div>
+                    // FadeIn(!collapsed, renderNavLinks())
+                } */}
+                    </div>
+                </animated.div>
             </div>
-        </header>
+        </div>
+
     );
 }
 
