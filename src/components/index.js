554928f9b2a656c7454ef8504f5516730ce45e97
nicholasprussen <59841031+nicholasprussen@users.noreply.github.com>
2022-01-07 10:41:38 +0000

    Deploying to gh-pages from @ scorchteam/scorchteam.github.io@326734242ab4b7890f1bfa22ee330a6b91c63108 🚀

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Header } from "./Header/Header";
-export { default as Footer } from "./Footer/Footer";
-
-export { default as Homepage } from "./Homepage/Homepage";
-export { default as About } from "./About/About";
-export { default as Contact } from "./Contact/Contact";
-export { default as Projects } from "./Projects/Projects";
